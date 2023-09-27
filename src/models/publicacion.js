22dbc7e7ba0debd03624885ff34c19b8c08eca66
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-09-27 01:12:27 -0300

    cambios modelos

---
+++ src/models/publicacion.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+// import mongoosePaginate from 'mongoose-paginate-v2';
+
+const schema = new mongoose.Schema({
+    tipo: {
+        type: String,
+        required: true,
+        enum: ['oferta', 'demanda'],
+    },
+    creador: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'usuarios',
+        required: true,
+    },
+    ini: { type: Date, required: true },
+    fin: { type: Date, required: true },
+    vigencia_desde: { type: Date, required: true },
+    vigencia_hasta: { type: Date, required: true },
+    observaciones: { type: String },
+    estado: {
+        type: String,
+        required: true,
+        enum: ['disponible', 'finalizada'], // FIXME: adaptar estados.
+    },
+
+    // Sólo para ofertas
+    cochera: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'cocheras',
+    },
+    precio: { type: Number },
+
+    // Sólo para demandas
+    vehiculo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'vehiculos',
+    },
+});
+
+// schema.plugin(mongoosePaginate);
+
+export default mongoose.model('publicaciones', schema);
--- src/models/usuario.js
+++ src/models/usuario.js
@@ -5,7 +5,7 @@ const schema = new mongoose.Schema({
     nombre: { type: String, required: true },
     apellido: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }, // FIXME: encriptar
     cbu: { type: String },
 });
 
