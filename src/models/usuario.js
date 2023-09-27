40efe45a3a8e8c67a414d0527468311f9f69107b
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-09-27 00:56:35 -0300

    normalización schema

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 // import mongoosePaginate from 'mongoose-paginate-v2';
 
-const usuariosSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
     nombre: { type: String, required: true },
     apellido: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
 });
 
-// productSchema.plugin(mongoosePaginate);
+// schema.plugin(mongoosePaginate);
 
-export default mongoose.model('usuarios', usuariosSchema);
+export default mongoose.model('usuarios', schema);
