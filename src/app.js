94023cf6526d755251f30cc36278efba7b0ddcac
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-09-27 01:40:44 -0300

    prueba renderizando front

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ const PORT = 8080;
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(process.cwd() + '/public'));
 
 // HTTP Server
 const httpServer = app.listen(PORT, () => {
@@ -25,3 +26,7 @@ const httpServer = app.listen(PORT, () => {
 
 // Routers
 setRouters(app);
+
+app.get('/', async (req, res) => {
+    res.sendFile('./public/index.html', { root: './' });
+});
