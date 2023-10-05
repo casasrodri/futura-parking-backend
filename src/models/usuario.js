7de56859933deb5020c8274d9cf39ed56594a3b7
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-10-05 01:24:11 -0300

    agregado telefono

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -7,6 +7,7 @@ const schema = new mongoose.Schema(
         apellido: { type: String, required: true },
         email: { type: String, required: true },
         password: { type: String, required: true }, // FIXME: encriptar
+        telefono: { type: Number, required: false },
         cbu: { type: String },
     },
     { timestamps: true }
